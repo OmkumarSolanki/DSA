2e0ffab40d7d6a7378d3679826fb4b96c5025a09
Omkumar Solanki <144753825+OmkumarSolanki@users.noreply.github.com>
2026-01-03 14:17:53 -0500

    Time: 0 ms (100.00%) | Memory: 12 MB (5.38%) - LeetSync

---
+++ 63-unique-paths-ii/unique-paths-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+private:
+    int calculateTotalPaths(int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>>& dp) {
+        if( i < 0 || j<0 ) {
+            return 0;
+        }
+        if(dp[i][j]!=-1) {
+            return dp[i][j];
+        }
+
+        if(obstacleGrid[i][j]==1) {
+            return 0;
+        }
+        if(i ==0 && j == 0) {
+            return 1;
+        }
+
+        return dp[i][j] = calculateTotalPaths(i-1, j, obstacleGrid, dp) + calculateTotalPaths(i, j-1, obstacleGrid, dp);
+    }
+public:
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -1));
+        return calculateTotalPaths(m-1, n-1, obstacleGrid, dp);
+
+    }
+};
