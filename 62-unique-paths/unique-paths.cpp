2d0db49edb523349b455a1c3e60d37002911ef04
Omkumar Solanki <144753825+OmkumarSolanki@users.noreply.github.com>
2026-01-03 13:41:55 -0500

    Time: 0 ms (100.00%) | Memory: 8.1 MB (84.49%) - LeetSync

--- 62-unique-paths/unique-paths.cpp
+++ 62-unique-paths/unique-paths.cpp
@@ -19,7 +19,7 @@ public:
 
         first row and first column there is only one way to reach there
         **/
-
+        /**
         vector<vector<int>> dp(m, vector<int>(n, -1));
         // first columne
         for(int i=0; i<m; i++) {
@@ -35,10 +35,33 @@ public:
                 dp[i][j] = dp[i-1][j] + dp[i][j-1];
             }
         }
-
         return dp[m-1][n-1];
+        **/
         // TC - O(m*n)
         // SC - O(m*n) - dp size
 
+        // Space Optimised Solution
+        /**
+
+        1  1. 1. 1
+        1  2. 3. 4
+        1. 3. 6. 10
+
+        dp[i][j] = dp[i-1][j] + dp[i][j-1]
+        If I look carefully then I am going column wise
+        and I need only two columns
+        **/
+
+        vector<int> prev(n, 1);
+        for(int i=1; i<m; i++) {
+            vector<int> curr(n, 1);
+            for(int j=1; j<n; j++) {
+                curr[j] = prev[j] + curr[j-1];
+            }
+            prev = curr;
+        }
+        return prev[n-1];
+        // TC - O(m*n)
+        // SC - O(n)
     }
 };
